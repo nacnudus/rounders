db1709176caceae3ed22854dbcbe7393ccb5dfab
Duncan Garmonsway <nacnudus@gmail.com>
2018-12-18 16:47:20 +0000

    Initial commit

---
+++ R/round.R
@@ -0,0 +1,47 @@
+#' Round to a Gap of Given Width
+#'
+#' @param x numeric or integer vector
+#' @param width defines how wide a 'gap' is
+#'
+#' @name round_gap
+#' @example
+#' floor_gap(c(5, 7, 8, 11, 15, 17, 25), gap = 3)
+#' ceiling_gap(c(5, 7, 8, 11, 15, 17, 25), gap = 3)
+NULL
+
+#' @rdname round_gap
+#' @export
+floor_gap <- function(x, width) {
+  first <- x[1]
+  last <- first
+  out <- vector(class(x), length(x))
+  for (i in seq_along(x)) {
+    if (x[i] - last < width) {
+      out[i] <- first
+    } else {
+      first <- x[i]
+      out[i] <- first
+    }
+    last <- x[i]
+  }
+  out
+}
+
+#' @rdname round_gap
+#' @export
+ceiling_gap <- function(x, width) {
+  x <- rev(x)
+  first <- x[1]
+  last <- first
+  out <- vector(class(x), length(x))
+  for (i in seq_along(x)) {
+    if (last - x[i] < width) {
+      out[i] <- first
+    } else {
+      first <- x[i]
+      out[i] <- first
+    }
+    last <- x[i]
+  }
+  rev(out)
+}
